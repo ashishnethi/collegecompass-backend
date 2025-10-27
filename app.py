2bfc1259dd9a558406c0b136600de8232f1c190e
ashishnethi <127278625+ashishnethi@users.noreply.github.com>
2025-10-27 15:48:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
-
+import os
 app = Flask(__name__)
 CORS(app)
 
@@ -79,4 +79,6 @@ def match():
     return jsonify(sorted_unis)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
